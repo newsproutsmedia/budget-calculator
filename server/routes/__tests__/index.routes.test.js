08755fe2788e1556a6ee061fe9dab4b6a10062c0
Nate Walters <newsproutsmedia@gmail.com>
2021-08-06 14:41:21 -0700

    Add express routes

--- server/routes/__tests__/index.routes.test.js
+++ server/routes/__tests__/index.routes.test.js
@@ -11,6 +11,24 @@ describe('GET /', () => {
   });
 });
 
+describe('GET /budget-calculator', () => {
+  it('should respond with 200 status to /budget-calculator endpoint and serve HTML', (done) => {
+    request(app)
+      .get('/budget-calculator')
+      .expect('Content-Type', 'text/html; charset=UTF-8')
+      .expect(200)
+      .end(done);
+  });
+
+  it('should respond with 200 status to /budget-calculator/* endpoints and serve HTML', (done) => {
+    request(app)
+      .get('/budget-calculator/app')
+      .expect('Content-Type', 'text/html; charset=UTF-8')
+      .expect(200)
+      .end(done);
+  });
+});
+
 describe('GET /api/test', () => {
   it('should respond with 200 status', (done) => {
     request(app)
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -2,7 +2,7 @@ const path = require('path');
 const logger = require('../logging/logger');
 
 module.exports = (app) => {
-  app.get('/', (req, res) => {
+  app.get(['/', '/budget-calculator', '/budget-calculator/*'], (req, res) => {
     res.status(200).sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
   });
 
