b81bf7fbd1613f35e2310ea486a88d436a866a66
Nate Walters <newsproutsmedia@gmail.com>
2021-08-06 14:45:13 -0700

    Fix lint errors on express routes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -8,12 +8,12 @@ module.exports = (app) => {
 
   app.get('/api/test', (req, res) => {
     logger.debug('This is the "/test" route.');
-    res.status(200).send(`You're connected to the backend!`);
+    res.status(200).send('You\'re connected to the backend!');
   });
 
   app.post('/api/message', (req, res) => {
     logger.debug(req.body);
-    const messageResponse = {'message': `POST received! You sent: "${req.body.message}"`};
+    const messageResponse = { message: `POST received! You sent: "${req.body.message}"` };
     res.status(200).send(messageResponse);
   });
 };
