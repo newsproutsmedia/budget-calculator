2cf071c87da1ca949bc268b848c9ef433d87314f
Nate Walters <newsproutsmedia@gmail.com>
2021-08-07 00:06:04 -0700

    Add home scaffold

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,36 @@ import {
   Link,
 } from 'react-router-dom';
 import Home from './pages/Home';
+import logo from './images/yardzen-logo.png';
 import Calculate from './pages/Calculate';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/calculate">Calculate</Link></li>
-          </ul>
+      <div id="app" className="app">
+        <nav className="header">
+          <brand className="brand">
+            <img src={logo} alt="Yardzen logo" title="Yardzen" className="logo" height="40" />
+          </brand>
+          <menu className="menu">
+            <ul className="menuList">
+              <li className="menuItem"><Link to="/">Home</Link></li>
+              <li className="menuItem"><Link to="/calculate">Calculate</Link></li>
+            </ul>
+          </menu>
         </nav>
+        <main className="main">
+          <Switch>
+            <Route path="/calculate">
+              <Calculate />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
       </div>
-      <Switch>
-        <Route path="/calculate">
-          <Calculate />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
     </Router>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as styles from './Home.module.css';
 
 function Home() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event) => {
+    setValue(event.currentTarget.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // set context value to 
+  };
+
   return (
-    <h1 id="home">Home Page</h1>
+    <div id="home" className={styles.home}>
+      <h1 className={styles.heading}>Budget Calculator</h1>
+      <h2>{value}</h2>
+      <div className={styles.formContainer}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input id="budgetInput" type="number" onChange={handleChange} className={styles.input} placeholder="Enter your budget" />
+          <input type="submit" value="Continue" className={styles.button} />
+        </form>
+      </div>
+    </div>
   );
 }
 
